33a448b527b3e42115eccfd8bfd569647673cd9d
Nishant Rai <nishant.rai@comprotechnologies.com>
2022-08-29 17:55:07 +0530

    Onboarding Backend: added swagger documentation for APIs

--- routes/dept.js
+++ routes/dept.js
@@ -11,19 +11,55 @@ router.get('/', (req, res) => {
 });
 router.post('/', (req, res) => {
   const dept = new Dept(req.body);
-  dept.save()
-    .then((deptRes) => res.json(deptRes))
-    .catch((err) => res.json(err));
+  dept.save((err, createdDept) => {
+    if (createdDept) {
+      res.status(201);
+      res.json(createdDept);
+      return;
+    }
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+    }
+  });
 });
 router.put('/:id', (req, res) => {
-  Dept.findOneAndUpdate({ _id: req.params.id }, req.body)
-    .then((updatedDept) => res.json(updatedDept))
-    .catch((err) => res.json(err));
+  Dept.findOneAndUpdate({ _id: req.params.id }, req.body, {}, (err, updatedDept) => {
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+      return;
+    }
+    if (!updatedDept) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.status(200);
+    res.json(updatedDept);
+    res.end();
+  });
+  // .then((updatedDept) => res.json(updatedDept))
+  // .catch((err) => res.json(err));
 });
 router.delete('/:id', (req, res) => {
-  Dept.findOneAndDelete({ _id: req.params.id })
-    .then((deletedDept) => res.json(deletedDept))
-    .catch((err) => res.json(err));
+  Dept.findOneAndDelete({ _id: req.params.id }, (err, deletedDept) => {
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+      return;
+    }
+    if (!deletedDept) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.status(200);
+    res.json(deletedDept);
+    res.end();
+  });
+  // .then((deletedDept) => res.json(deletedDept))
+  // .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- routes/task.js
+++ routes/task.js
@@ -11,25 +11,68 @@ router.get('/', (req, res) => {
 });
 router.get('/:id', (req, res) => {
   Task.findOne({ _id: req.params.id }, (err, task) => {
-    if (task) res.json(task);
-    else res.json({ error: err });
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+      return;
+    }
+    if (!task) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.status(200);
+    res.json(task);
+    res.end();
   });
 });
 router.post('/', (req, res) => {
   const task = new Task(req.body);
-  task.save()
-    .then((createdTask) => res.json(createdTask))
-    .catch((err) => res.json(err));
+  task.save((err, createdTask) => {
+    if (createdTask) {
+      res.status(201);
+      res.json(createdTask);
+      return;
+    }
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+    }
+  });
 });
 router.put('/:id', (req, res) => {
-  Task.findOneAndUpdate({ _id: req.params.id }, req.body)
-    .then((updatedTask) => res.json(updatedTask))
-    .catch((err) => res.json(err));
+  Task.findOneAndUpdate({ _id: req.params.id }, req.body, {}, (err, updatedTask) => {
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+      return;
+    }
+    if (!updatedTask) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.status(200);
+    res.json(updatedTask);
+    res.end();
+  });
 });
 router.delete('/:id', (req, res) => {
-  Task.findOneAndDelete({ _id: req.params.id })
-    .then((deletedTask) => res.json(deletedTask))
-    .catch((err) => res.json(err));
+  Task.findOneAndDelete({ _id: req.params.id }, {}, (err, deletedTask) => {
+    if (err) {
+      res.status(400);
+      res.json({ error: err });
+      return;
+    }
+    if (!deletedTask) {
+      res.status(404);
+      res.end();
+      return;
+    }
+    res.status(200);
+    res.json(deletedTask);
+    res.end();
+  });
 });
 
 module.exports = router;
